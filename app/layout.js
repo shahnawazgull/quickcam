6d1c3134dc8407f95d801b6d6e1e9dca6b6497c2
shahnawaz <shahnawazgull2166@gmail.com>
2025-03-08 02:47:15 +0500

    a

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css"
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.png"/>
       <body>
         {children}
       </body>
